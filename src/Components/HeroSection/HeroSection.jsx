631588505549020245ac687f8a5fac42838f2966
Zero <kahonide@gmail.com>
2022-05-15 18:52:57 +0200

    container

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -4,17 +4,20 @@ import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <div className="HeroContainer">
+      <img src="/img/waves.png" alt="" className="background" />
+
       <div className="heading-container">
         <h1 className="hero-heading">Time for sushi</h1>
       </div>
-      <img src="/img/waves.png" alt="" className="background" />
+
       <div className="sushi-img-container">
-        <img src="/img/splash.png" alt="" className="splash-img" />
-        <div className="animated-img">
-          <img src="/img/sushi.png" alt="" className="sushi-img" />
+        <div className="sushi-img-stamp-animation">
+          <img src="/img/splash.png" alt="" className="splash-img" />
+          <div className="animated-img">
+            <img src="/img/sushi.png" alt="" className="sushi-img" />
+          </div>
         </div>
       </div>
-      <div className="animated-arrow"></div>
     </div>
   );
 };
